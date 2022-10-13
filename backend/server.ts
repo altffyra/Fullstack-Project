97143f67f7800831fb25880833207f34aebee438
David Andersson <david.andersson@zocom.se>
2022-10-13 15:18:33 +0200

    Express serving static frontend files

--- backend/server.ts
+++ backend/server.ts
@@ -5,11 +5,22 @@ const app = express()
 import { Fruit, fakeData } from './fakeDb.js'
 const PORT = 1337  // TODO: environment variables istället
 
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url));  // __dirname i äldre versioner av node
+const staticPath = join( __dirname, '../../dist' )
 
 
 // Middleware
 app.use( express.json() )
 
+app.use( (req, res, next) => {
+	console.log(`${req.method}  ${req.url} `, req.body)
+	next()
+} )
+
+// Obs! express.static bör ligga först, när man får många statiska filer
+app.use( express.static(staticPath) )
 
 
 // Routes / endpoints
