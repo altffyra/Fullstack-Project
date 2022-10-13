bbb78e52afa768f821d32d869a56ea647933cff6
David Andersson <david.andersson@zocom.se>
2022-10-13 14:10:32 +0200

    POST /api/fruits

--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,12 @@ const app = express()
 import { Fruit, fakeData } from './fakeDb.js'
 const PORT = 1337  // TODO: environment variables istället
 
+
+
 // Middleware
+app.use( express.json() )
+
+
 
 // Routes / endpoints
 // Exempel: frontend skickar "GET /api/data", backend tar emot och servar ett svar
@@ -47,6 +52,57 @@ app.get('/api/fruits/:id', (req: IdParam, res) => {
 })
 
 // POST /api/fruits
+app.post('/api/fruits', (req, res) => {
+	let newFruit: Fruit = req.body
+	// Kontrollera att frukten matchar typen Fruit med hjälp av valideringsfunktion (TypeScript kan inte göra det)
+	// Om datan är i ett felaktigt format, skicka statuskod 400: Bad request
+	// Om allt gått bra, statuskod 200
+	// Observera att man måste köra rätt middleware, för att inkommande data ska läggas i req.body
+
+	console.log('POST incoming fruit: ', newFruit)
+	if( !newFruit ) {
+		// Svara med statuskod och informativt meddelande
+		res.status(400).send('No data')
+
+	} else if( isValidFruit(newFruit) ) {
+		// lägg till och skicka tillbaka statuskod 200
+		fakeData.fruits.push(newFruit)
+		res.sendStatus(200)
+
+	} else {
+		res.status(400).send('Bad fruit')
+	}
+})
+
+function isValidFruit(maybeFruit: Fruit): boolean {
+	// En giltig (valid) frukt måste ha egenskaperna name, price och id.
+	// Tips: kan göras i en loop
+	if (maybeFruit.hasOwnProperty('name')) {
+		if ((typeof maybeFruit.name !== 'string') || maybeFruit.name === '') {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	if (maybeFruit.hasOwnProperty('price')) {
+		if ((typeof maybeFruit.price !== 'number') || maybeFruit.price < 0 ) {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	if (maybeFruit.hasOwnProperty('id')) {
+		if ((typeof maybeFruit.id !== 'string') || maybeFruit.id === '') {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	return true
+}
 
 
 
