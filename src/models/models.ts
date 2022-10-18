dd37a28c7e32dcf66f4ef459dfb6393d395f7abd
David Andersson <lejonmanen@gmail.com>
2022-10-18 13:56:50 +0200

    Proxy server for frontend

---
+++ src/models/models.ts
@@ -0,0 +1,11 @@
+export type Fruit = {
+	name: string;
+	price: number;
+	id: string;
+}
+export type Schema = {
+	fruits: Fruit[];
+	// Lägg till mer data eftersom
+	// account: Account;
+	// orders: Order[];
+}
